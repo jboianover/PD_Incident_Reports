0def3469498e3df6b7550d359d542617a7c2784a
Jonathan Boianover <boia.jonathan@gmail.com>
2023-09-26 21:32:18 -0300

    dev: added transformation on dfs.

--- python-app/main.py
+++ python-app/main.py
@@ -1,15 +1,60 @@
 import pandas as pd
+import numpy as np
 from sqlalchemy import create_engine
+import os
 
 
-def _read_jsonl(file):
+def _read_jsonl(file_path):
+
+    schema = {
+        "incident_datetime": "datetime64[ns]",
+        "incident_date": "datetime64[ns]",
+        "incident_time": "object",
+        "incident_year": "int64",
+        "incident_day_of_week": "object",
+        "report_datetime": "datetime64[ns]",
+        "row_id": "int64",
+        "incident_id": "int64",
+        "incident_number": "int64",
+        "cad_number": "object",
+        "report_type_code": "object",
+        "report_type_description": "object",
+        "filed_online": "object",
+        "incident_code": "object",
+        "incident_category": "object",
+        "incident_subcategory": "object",
+        "incident_description": "object",
+        "resolution": "object",
+        "intersection": "object",
+        "cnn": "object",
+        "police_district": "object",
+        "analysis_neighborhood": "object",
+        "supervisor_district": "int64",
+        "latitude": "float64",
+        "longitude": "float64",
+        "point": "object"
+    }
+
     # Read the JSONL file into a pandas DataFrame
-    df = pd.read_json(file, lines=True)
+    df = pd.read_json(file_path, lines=True, dtype=schema)
 
     return df
 
 
 def _transform_df(df):
+    null_handling_cols = ['analysis_neighborhood', 'cad_number', 'supervisor_district',
+                          'filed_online']
+    for col in null_handling_cols:
+        df[col].replace(['', 'null'], np.nan, inplace=True)
+    # df['cad_number'].replace(['', 'null'], np.nan, inplace=True)
+    # df['filed_online'].replace('', 'false', inplace=True)
+    df = df[['row_id', 'incident_id', 'incident_number', 'cad_number', 'incident_datetime',
+             'incident_date', 'incident_time', 'incident_year', 'incident_day_of_week',
+             'report_datetime', 'report_type_code', 'report_type_description', 'filed_online',
+             'incident_code', 'incident_category', 'incident_subcategory', 'incident_description',
+             'resolution', 'intersection', 'cnn', 'police_district', 'analysis_neighborhood',
+             'supervisor_district', 'latitude', 'longitude', 'point']]
+
     return df
 
 
@@ -17,7 +62,7 @@ def _load_data(df, database_url):
     try:
         engine = create_engine(database_url)
         # Insert data into the database
-        df.to_sql('crime_reports', engine, if_exists='replace', index=False)
+        df.to_sql('crimes', engine, if_exists='append', index=False)
     except Exception as e:
         print(f"Error: {e}")
 
@@ -31,7 +76,8 @@ def process_and_insert_data():
     print(database_url)
 
     # Specify path/filename of source data file
-    file_path = 'sf_crime_reports.jsonl'
+    file_path = os.path.abspath('sf_crime_reports.jsonl')
+    print(f'file_path: {file_path}')
     df = _read_jsonl(file_path)
     final_df = _transform_df(df)
     _load_data(final_df, database_url)
