99d1ef6390fbede1d2fecac366e220c6e5f9593e
Jonathan Boianover <boia.jonathan@gmail.com>
2023-09-26 22:31:29 -0300

    dev: config file used within the script.

--- python-app/main.py
+++ python-app/main.py
@@ -2,64 +2,32 @@ import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
 import os
+import logging
+import json
 
 
-def _read_jsonl(file_path):
-
-    schema = {
-        "incident_datetime": "datetime64[ns]",
-        "incident_date": "datetime64[ns]",
-        "incident_time": "object",
-        "incident_year": "int64",
-        "incident_day_of_week": "object",
-        "report_datetime": "datetime64[ns]",
-        "row_id": "int64",
-        "incident_id": "int64",
-        "incident_number": "int64",
-        "cad_number": "object",
-        "report_type_code": "object",
-        "report_type_description": "object",
-        "filed_online": "object",
-        "incident_code": "object",
-        "incident_category": "object",
-        "incident_subcategory": "object",
-        "incident_description": "object",
-        "resolution": "object",
-        "intersection": "object",
-        "cnn": "object",
-        "police_district": "object",
-        "analysis_neighborhood": "object",
-        "supervisor_district": "int64",
-        "latitude": "float64",
-        "longitude": "float64",
-        "point": "object"
-    }
+def _read_jsonl(file_path, file_schema):
 
     # Read the JSONL file into a pandas DataFrame
-    df = pd.read_json(file_path, lines=True, dtype=schema)
+    df = pd.read_json(file_path, lines=True, dtype=file_schema)
 
     return df
 
 
-def _transform_df(df):
+def _transform_df(df, table_columns):
     # Clean up of empty or 'null' string values to be null in the table
     df = df.replace(['', 'null'], np.nan)
     # Re-ordering columns to match the order of the table to then optimize the insert
-    df = df[['row_id', 'incident_id', 'incident_number', 'cad_number', 'incident_datetime',
-             'incident_date', 'incident_time', 'incident_year', 'incident_day_of_week',
-             'report_datetime', 'report_type_code', 'report_type_description', 'filed_online',
-             'incident_code', 'incident_category', 'incident_subcategory', 'incident_description',
-             'resolution', 'intersection', 'cnn', 'police_district', 'analysis_neighborhood',
-             'supervisor_district', 'latitude', 'longitude', 'point']]
+    df = df[table_columns]
     return df
 
 
-def _load_data(df, database_url):
+def _load_data(df, database_url, table_name):
     try:
         # Create PostgresSQL connection
         engine = create_engine(database_url)
         # Insert data into the crimes table in the database
-        df.to_sql('crimes', engine, if_exists='append', index=False)
+        df.to_sql(table_name, engine, if_exists='append', index=False)
     except Exception as e:
         print(f"Error: {e}")
 
@@ -67,16 +35,32 @@ def _load_data(df, database_url):
 
 
 def process_and_insert_data():
-    # Read the DATABASE_URL environment variable
-    database_url = 'postgresql://admin:admin@localhost:5433/PD_Incidents'
-    print(database_url)
+
+    # Defining a config file
+    TABLE_CONFIG_FILE = 'config.json'
+    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+    # Reading config file to get the parameters of the process
+    with open(os.path.join(BASE_DIR, TABLE_CONFIG_FILE)) as config_file:
+        logging.info("Loading Configurations...")
+        CONFIG_FILE = json.load(config_file)
+
+    # Defining variables to use during the process
+    database_url = CONFIG_FILE['database_url']
+    table_name = CONFIG_FILE['table_name']
+    table_columns = CONFIG_FILE['table_columns']
+    file_name = CONFIG_FILE['file_name']
+    file_schema = CONFIG_FILE['file_schema']
 
     # Specify path/filename of source data file
-    file_path = os.path.abspath('sf_crime_reports.jsonl')
-    print(f'file_path: {file_path}')
-    df = _read_jsonl(file_path)
-    final_df = _transform_df(df)
-    _load_data(final_df, database_url)
+    file_path = os.path.abspath(file_name)
+    logging.info(f'file_path: {file_path}')
+    # Function to get a Pandas Dataframe after reading jsonl source file
+    df = _read_jsonl(file_path, file_schema)
+    # Function with the transformations logic returning the df to be inserted
+    final_df = _transform_df(df, table_columns)
+    # Function to write the final df to the target table
+    _load_data(final_df, database_url, table_name)
 
 
 if __name__ == "__main__":
